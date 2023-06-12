53554eb06e688339d0c5c9354f2e648d642291a2
AkmalKurbanov <250296a@gmail.com>
2023-06-13 01:54:16 +0600

    end

--- app/js/media.js
+++ app/js/media.js
@@ -1,6 +1,9 @@
 if (window.matchMedia('(max-width: 767px)').matches) {
   $('.hero__callback').before($('.hero__arch'));
   $('.reviews__slider').before($('.reviews__arch'));
+  $('.about__text').before($('.about__img'));
+  $('.about__count-wrap').after($('.about__btn-wrap'));
+  
 }
 
 
